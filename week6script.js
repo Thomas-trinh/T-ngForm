04faebf074eb496feb435fa4c98339af683fa396
Phong <phamkhacthanhphong@gmail.con>
2023-04-14 21:51:38 +0700

    first commit

--- week6script.js
+++ week6script.js
@@ -2,34 +2,42 @@ const InDates = document.getElementById('InDates')
 const InCates = document.getElementById('InCates')
 const InDes = document.getElementById('InDes')
 const InAmount = document.getElementById('InAmount')
+const InAmount1 = document.getElementById('InAmount1')
+
 const MoneyForm = document.getElementById('MoneyForm')
 
 MoneyForm.addEventListener('submit', (e) => {
-    let formInvalid = false;
-    let message = []
-    if (!InDates.value) {
-        message.push('Date is required!');
-        formInvalid = true;
-    }
-    if (!InAmount.value) {
-        message.push('Amount is required!');
-        formInvalid = true;
-    }
-    if (InAmount.value < 0) {
-        message.push('Income can not be negative!');
-        formInvalid = true;
-    }
+    // let formInvalid = false;
+    // let message = []
+    // if (!InDates.value) {
+    //     message.push('Date is required!');
+    //     formInvalid = true;
+    // }
+    // if (!InAmount.value) {
+    //     message.push('Amount is required!');
+    //     formInvalid = true;
+    // }
+    // if (InAmount.value < 0) {
+    //     message.push('Income can not be negative!');
+    //     formInvalid = true;
+    // }
 
-    // if(formInvalid) {
-    //     e.preventDefault(false);
-    // } 
+    // // if(formInvalid) {
+    // //     e.preventDefault(false);
+    // // } 
 
-    if (message.length > 0) {
-        e.preventDefault();
-        errorElement.innerHTML = message.join(", ")
-    }
-
+    // if (message.length > 0) {
+    //     e.preventDefault();
+    //     errorElement.innerHTML = message.join(", ")
+    // }
 })
+
+function CalculateTotalIn(){
+  let total = 0;
+  total += parseInt(InAmount.value);
+  total += parseInt(InAmount1.value);
+  document.getElementById("TotalIn").value = total;
+}
 
 function ValidateYear(obj, msg) {
     var userInput = obj.value;
@@ -43,4 +51,27 @@ function ValidateYear(obj, msg) {
       }
   
     return;
+}
+
+function ValidateDescription(obj, msg) {
+  var userInput = obj.value;
+  if (userInput.length != 0) {
+    msg.innerHTML = "";
+  }
+  else
+    msg.innerHTML = "";
+
+  return;
+}
+
+
+function ValidateAmount(obj, msg) {
+  var userInput = obj.value;
+  if (userInput.length != 0) {
+    msg.innerHTML = "";
+  }
+  else
+    msg.innerHTML = "";
+
+  return;
 }
